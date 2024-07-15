745d30c063d07e78aa0675b2f9ec8e9f3465db0a
Cameron Dehning <bedsteler2.0@gmail.com>
2024-07-15 17:49:21 -0500

    wip

---
+++ build.rs
@@ -0,0 +1,13 @@
+use std::env;
+
+pub fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    // check if a feature is enabled
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_language(cbindgen::Language::C)
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("target/include/cutelight.h");
+}
